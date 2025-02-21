cb1b3a7dcacaf2375064a1754fbb21c5ea270ad3
vigneshs-dev <vigneshkavery97@gmail.com>
2025-02-21 23:44:18 +0530

    Update complete ci-vote.yml file

--- vote/app.py
+++ vote/app.py
@@ -7,7 +7,7 @@ import json
 import logging
 
 option_a = os.getenv("OPTION_A", "Cats")
-option_b = os.getenv("OPTION_B", "Dogs")
+option_b = os.getenv("OPTION_B", "Dog")
 hostname = socket.gethostname()
 
 app = Flask(__name__)
